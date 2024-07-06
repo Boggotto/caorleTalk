a34368a9cd75b6f79975a7526aa36f798eb02ae9
Francesco Gobbato <francescogobbato00@gmail.com>
2024-07-06 19:24:47 +0200

    commit

--- caorleTalk/src/app/app.component.ts
+++ caorleTalk/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { RouterModule } from '@angular/router';
+import { ContattiComponent } from './components/contatti/contatti.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      RouterOutlet, 
+      RouterModule,
+      HomeComponent,
+      ContattiComponent,
+      HeaderComponent
+      ]
 })
 export class AppComponent {
   title = 'caorleTalk';
--- caorleTalk/src/app/app.routes.ts
+++ caorleTalk/src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { ContattiComponent } from './components/contatti/contatti.component';
+import { NewsComponent } from './components/news/news.component';
+import { PodcastComponent } from './components/podcast/podcast.component';
+import { SocialComponent } from './components/social/social.component';
+import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, title: "Home Page" },
+    { path: 'contatti', component: ContattiComponent, title: "Contatti" },
+    { path: 'news', component: NewsComponent, title: "News" },
+    { path: 'podcast', component: PodcastComponent, title: "PodCast" },
+    { path: 'social', component: SocialComponent, title: "Social" },
+    { path: 'chi-siamo', component: ChiSiamoComponent, title: "Chi Siamo" }
+];
+
+@NgModule({
+    providers: [
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+    ],
+    imports: [RouterModule.forRoot(routes,{ useHash: true })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
