f98a6e48e00ad116eb9393c1c9c35066c36d9d4f
Francesco Gobbato <francescogobbato00@gmail.com>
2024-08-12 14:01:45 +0200

    commit

--- caorleTalk/src/app/app.component.ts
+++ caorleTalk/src/app/app.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
-import { RouterModule } from '@angular/router';
-import { ContattiComponent } from './components/contatti/contatti.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from "./layout/footer/footer.component";
+import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Location } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
-    standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [
-        RouterOutlet,
-        RouterModule,
-        HomeComponent,
-        ContattiComponent,
-        HeaderComponent,
-        FooterComponent
-    ]
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'caorleTalk';
+export class AppComponent implements OnInit {
+    private _router: Subscription;
+    @ViewChild(NavbarComponent) navbar: NavbarComponent;
+
+    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    ngOnInit() {
+        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+            if (window.outerWidth > 991) {
+                window.document.children[0].scrollTop = 0;
+            }else{
+                window.document.activeElement.scrollTop = 0;
+            }
+            this.navbar.sidebarClose();
+
+            this.renderer.listen('window', 'scroll', (event) => {
+                const number = window.scrollY;
+                var _location = this.location.path();
+                _location = _location.split('/')[2];
+
+                if (number > 150 || window.pageYOffset > 150) {
+                    navbar.classList.remove('navbar-transparent');
+                } else if (_location !== 'login' && this.location.path() !== '/nucleoicons') {
+                    // remove logic
+                    navbar.classList.add('navbar-transparent');
+                }
+            });
+        });
+    }
 }
---
+++ caorleTalk/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingComponent } from './examples/landing/landing.component';
+
+const routes: Routes =[
+    { path: '', pathMatch: 'full' , component: LandingComponent},
+    //{ path: 'index',                component: ComponentsComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
