e00e120870524b126f9386903dfee9cc7bfa54b7
Francesco Gobbato <francescogobbato00@gmail.com>
2024-08-13 19:43:11 +0200

    commit

--- caorleTalk/src/app/examples/contatti/contatti.component.ts
+++ caorleTalk/src/app/examples/contatti/contatti.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 import * as Rellax from 'rellax';
 
 @Component({
@@ -11,7 +13,13 @@ export class ContattiComponent implements OnInit {
   focus;
   focus1;
 
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    from_name: '',
+    from_email: '',
+    message:''
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     var rellaxHeader = new Rellax('.rellax-header');
@@ -27,4 +35,17 @@ export class ContattiComponent implements OnInit {
     var navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.remove('navbar-transparent');
   }
+
+  async send(){
+    emailjs.init('xK1jsXm72plxg8Hqz');
+    let response = await emailjs.send("service_dtj4m43","template_9sj627j",{
+      from_name: this.form.value.from_name,
+      from_email: this.form.value.from_email,
+      message: this.form.value.message,
+      });
+      
+      alert('Messaggio inviato');
+      this.form.reset();
+  }
+
 }
--- caorleTalk/src/app/examples/examples.module.ts
+++ caorleTalk/src/app/examples/examples.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NouisliderModule } from 'ng2-nouislider';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
@@ -18,6 +18,7 @@ import { PodcastComponent } from './podcast/podcast.component';
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         NgbModule,
         NouisliderModule,
         JwBootstrapSwitchNg2Module,
